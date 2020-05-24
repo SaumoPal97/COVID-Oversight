1141edc1a352fca59f249947dacf9cec5810f25b
Saumo Pal <saumopal1997@iitkgp.ac.in>
2020-05-25 00:46:58 +0530

    Update trial.py

--- flask-app/trial.py
+++ flask-app/trial.py
@@ -2,10 +2,10 @@ from twilio.rest import Client
 
 
 def twilio_sms(text):
-    account_sid = "AC9076dbe76f82eb68aea596337f4d1f31"
-    auth_token = "7b7d29f6482c8dd420e91506fc23e6a8"
-    my_num = "+918016598546"
-    twilio_num = "+12513571209"
+    account_sid = "<ACCOUNT-SID>"
+    auth_token = "<AUTH-TOKEN>"
+    my_num = "<PHONE-NUM>"
+    twilio_num = "<TWILIO-NUM>"
     client = Client(account_sid, auth_token)
     sms = client.messages.create(
         from_= twilio_num,
